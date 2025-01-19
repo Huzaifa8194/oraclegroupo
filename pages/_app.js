26d3d69dd56edcfaec61128d2e177168d89191d1
huzaifa8194 <huzaifa8195@gmail.com>
2025-01-20 01:32:09 +0500

    name changes

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <title>Orgarium - Agriculture Farming React Template</title>
+        <title>Oracle Development Group</title>
         {/*====== Favicon Icon ======*/}
         <link
           rel="shortcut icon"
